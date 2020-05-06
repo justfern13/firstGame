5220fddcde71d834451dd7df75a9c1acd5bf139f
Justin Fernandez <justin.rd.fernandez@hotmail.com>
2020-05-06 09:27:37 -0700

    Added HP checks

--- Battle.java
+++ Battle.java
@@ -14,44 +14,64 @@ public class Battle {
       enemy.setAtk(2);
       enemy.setMaxHp(5);
       enemy.setCurrentHp(enemy.getMaxHp());
-      
       System.out.println("You have run into a " + enemy.getName());
       System.out.println("Get ready for battle!!!");
-      System.out.println();
+      breaks(1);
 
       while(inBattle) {
-         currentStatus();
-         String choice = userChoice();
-         //Creature attacker = checkPriority(player, enemy);
-
-         if (choice.toLowerCase().equals("a")) {
-            
-            if (player.getSpd() >= enemy.getSpd()) {
-               System.out.println("You swing your valiant pooperscoop and hit the " + enemy.getName());
-               enemy.setCurrentHp(enemy.getCurrentHp() - (player.getAtk() - enemy.getDef()));
-               System.out.println("You suck and get hit by " + enemy.getName() + "'s peenyweeny");
-               System.out.println();
-               player.setCurrentHp(player.getCurrentHp() - (enemy.getAtk() - enemy.getDef()));
-            }
-         } else if (choice.toLowerCase().equals("d")) {
-            System.out.println("You defend! No damage taken.");
-            System.out.println();
-         } else if (choice.toLowerCase().equals("r")) {
-            System.out.println("You a bitch.");
-            inBattle = false;
-         } else {
-            System.out.println("Yo bro try again.");
+         //Checks HP of both player and enemy and breaks if either die
+         if (player.getCurrentHp() == 0) {
+            System.out.println("You died...You suck");
+            breaks(2);
+            player.setCurrentHp(player.getMaxHp());
+            break;
+         } else if (enemy.getCurrentHp() == 0) {
+            System.out.println("You killed " + enemy.getName() + ". You gained a level");
+            breaks(2);
+            break;
          }
-      }    
+         
+         currentStatus();
+         
+         //Decision loop for turn
+         String choice = userChoice();
+         Creature priority = checkPriority(player, enemy);
+         if (priority == player) {
+            if (choice.toLowerCase().equals("a")) {
+                  if (player.getSpd() >= enemy.getSpd()) {
+                     System.out.println("You swing your valiant pooperscoop and hit the " + enemy.getName());
+                     enemy.setCurrentHp(enemy.getCurrentHp() - (player.getAtk() - enemy.getDef()));
+                     System.out.println("You suck and get hit by " + enemy.getName() + "'s peenyweeny");
+                     breaks(1);
+                     player.setCurrentHp(player.getCurrentHp() - (enemy.getAtk() - enemy.getDef()));
+                  }
+            } else if (choice.toLowerCase().equals("d")) {
+               System.out.println("You defend! No damage taken.");
+               breaks(1);
+            } else if (choice.toLowerCase().equals("r")) {
+               System.out.println("You a bitch.");
+               inBattle = false;
+            } else {
+               System.out.println("Yo bro try again.");
+               breaks(2);
+               
+            }
+         }
+      } 
    }
 
+   public void breaks(int x) {
+      for (int i = 0; i < x; i++) {
+         System.out.println();
+      }
+   }
+      
    public void currentStatus() {
       player.showStatus();
       System.out.println();
       enemy.showStatus();
       System.out.println();      
    }
-
 
    public String userChoice () {
       System.out.println("What would you like to do?");
@@ -88,52 +108,4 @@ public class Battle {
          return player;
       }     
    }  
-      
-     /** while(inBattle) {
-         boolean turnChoice = true;
-         while(turnChoice) {
-            System.out.println("What would you like to do?");
-            System.out.println("A for Attack");
-            System.out.println("D for Defend");
-            System.out.println("R for Run");
-            Scanner userInput = new Scanner(System.in);
-            String battleChoice = userInput.nextLine();
-            System.out.println();
-            if (battleChoice.toLowerCase().equals("a")) {
-               if (mainChar.getSpd() >= enemy.getSpd()) {
-                  System.out.println("You swing your valiant pooperscoop and hit the " + enemy.getName());
-                  enemy.setCurrentHp(enemy.getCurrentHp() - (mainChar.getAtk() - enemy.getDef()));
-                  System.out.println("You suck and get hit by " + enemy.getName() + "'s peenyweeny");
-                  System.out.println();
-                  mainChar.setCurrentHp(mainChar.getCurrentHp() - (enemy.getAtk() - enemy.getDef()));
-                  mainChar.showStatus();
-                  enemy.showStatus();
-               }
-            } else if (battleChoice.toLowerCase().equals("d")) {
-               System.out.println("You defend! No damage taken.");
-               System.out.println();
-               mainChar.showStatus();
-               enemy.showStatus();
-            } else if (battleChoice.toLowerCase().equals("r")) {
-               System.out.println("You a bitch.");
-               turnChoice = false;
-               inBattle = false;
-            } else {
-               System.out.println("Yo bro try again.");
-            }
-               
-            if(mainChar.getCurrentHp() == 0) {
-               System.out.println("Damn bro. You suck. And you died. LOL");
-               mainChar.setCurrentHp(mainChar.getMaxHp());
-               turnChoice = false;
-               inBattle = false;
-            } else if(enemy.getCurrentHp() == 0) {
-               System.out.println("Cool story bro. You win. Have a level.");
-               mainChar.gainLvl();
-               mainChar.showAllStats();
-               turnChoice = false;
-               inBattle = false;
-            }  
-         }
-         */
 }
